d9a4278a6aa23284d028496f066b5c4d022c9787
Linage <p.anastasia.danu@gmail.com>
2024-07-26 10:24:53 +0300

    get info of pet

--- src/components/IdSection.vue
+++ src/components/IdSection.vue
@@ -2,40 +2,65 @@
     <div class="dog-info">
         <div class="container d-flex">
             <div>
-                <img src="../../public/Logo.svg" alt="Dog Picture">
+                <img :src='pet.image_link' alt="Pet Picture">
             </div>
             <div>
-                <p><strong>Name: </strong>{{ name }}</p>
-                <p><strong>Surname: </strong>{{ surname }}</p>
-                <p><strong>Vârstă: </strong>{{ age }}</p>
-                <p><strong>Vaccin: </strong>{{ vaccin }}</p>
-                <p class="width1"><strong>Biografie: </strong><br/>{{ bio }}</p>
+                <p><strong>Name: </strong>{{ pet.name }}</p>
+                <p><strong>Surname: </strong>{{ pet.surname }}</p>
+                <p><strong>Vârstă: </strong>{{ pet.age }} ani</p>
+                <p><strong>Vaccin: </strong>
+                    <span v-if="pet.vaccine==false">Nu</span>
+                    <span v-else>Da</span>
+                </p>
+                <p class="width1"><strong>Biografie: </strong><br/>{{ pet.bio }}</p>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script>    
+    import { myaxios } from '../axios';
     export default {
         data() {
             return {
                 name: '',
                 surname: '',
                 type: 0,
-                age: 2,
+                age: 0,
                 image_link: '',
                 vaccine: true,
                 bio: '',
+                pet: {},
             }
         },
+        computed: {
+            id() {
+                return this.$route.params.id; 
+            }
+        },
+        methods: {            
+            getPets() {
+                /*myaxios.get(`/pet`).then(
+                    (data) => {
+                        console.log(data);
+                    },
+                );*/
+                myaxios.get(`/pet/${this.id}`).then(
+                    (data) => {
+                        this.pet = data.data;
+                        console.log(this.pet);
+                    },
+                );
+            },
+        },
         mounted () {
-            ;
+            this.getPets();
         },
     }
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kalam:wght@300;400;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400..700;1,400..700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Rakkas&display=swap');
+    @import url('https://fonts.googleapis.com/css2?wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Rakkas&display=swap');
 
     * {
         font-family: "Merriweather", sans-serif;
