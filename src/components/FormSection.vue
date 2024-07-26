c6939822dc07df186e8459cd4cdfe3e032c9b87a
Linage <p.anastasia.danu@gmail.com>
2024-07-26 11:27:28 +0300

    FormSection and IdSection update 3

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -5,24 +5,24 @@
         <div class="form">
             <div class="box grid">
                 <label for="nume">Nume:</label>
-                <input type="text" id="nume" name="nume" v-model="name"/>
+                <input type="text" id="nume" name="nume" v-model="name" required/>
             </div>
 
             <div class="box grid">
                 <label for="prenume">Prenume:</label>
-                <input type="text" id="prenume" name="prenume" v-model="surname"/>
+                <input type="text" id="prenume" name="prenume" v-model="surname" required/>
             </div>
 
             <div class="box grid">
                 <label for="telefon">Nr. de telefon:</label>
-                <input type="tel" id="telefon" name="telefon" v-model="phone"/>
+                <input type="tel" id="telefon" name="telefon" v-model="phone" required/>
             </div>
 
             <h2 class="color-g">Experiențe și preferințe</h2>
 
             <div class="box grid">
                 <label for="locatie">Unde locuiți?</label>
-                <select name="locatie" v-model="location">
+                <select name="locatie" v-model="house_type" required>
                     <option value="apartament">Apartament</option>
                     <option value="casa pe pamant">Casa pe pământ</option>
                 </select>
@@ -32,10 +32,10 @@
                 <div class="radio-group ">
                     <label>Ați mai avut animale de companie? Câte?</label>
                     <div class="d-flex">
-                        <label><input type="radio" name="animale" value="Nu" v-model="petbefore"> Nu</label>
-                        <label><input type="radio" name="animale" value="1" v-model="petbefore"> 1</label>
-                        <label><input type="radio" name="animale" value="2" v-model="petbefore"> 2</label>
-                        <label><input type="radio" name="animale" value="3+" v-model="petbefore"> 3+</label>
+                        <label><input type="radio" name="animale" value="Nu" v-model="pets"> Nu</label>
+                        <label><input type="radio" name="animale" value="1" v-model="pets"> 1</label>
+                        <label><input type="radio" name="animale" value="2" v-model="pets"> 2</label>
+                        <label><input type="radio" name="animale" value="3+" v-model="pets"> 3+</label>
                     </div>
                 </div>
             </div>
@@ -44,8 +44,8 @@
                 <div class="radio-group">
                     <label>Sunteți conștient/ă de costurile viitoare necesare?</label>
                     <div class="d-flex">
-                        <label><input type="radio" name="costuri" value="true" v-model="costs"> Da</label>
-                        <label><input type="radio" name="costuri" value="false" v-model="costs"> Nu</label>
+                        <label><input type="radio" name="costuri" value="true" v-model="cost_agreement"> Da</label>
+                        <label><input type="radio" name="costuri" value="false" v-model="cost_agreement"> Nu</label>
                     </div>
                 </div>
             </div>
@@ -54,10 +54,10 @@
                 <div class="radio-group">
                     <label>Câte ore pe zi sunteți acasă?</label>
                     <div class="d-flex">
-                        <label><input type="radio" name="ore" value="3-4" v-model="stayathome"> 3-4</label>
-                        <label><input type="radio" name="ore" value="5-7" v-model="stayathome"> 5-7</label>
-                        <label><input type="radio" name="ore" value="7-9" v-model="stayathome"> 7-9</label>
-                        <label><input type="radio" name="ore" value="12+" v-model="stayathome"> 12+</label>
+                        <label><input type="radio" name="ore" value="3-4" v-model="free_hours"> 3-4</label>
+                        <label><input type="radio" name="ore" value="5-7" v-model="free_hours"> 5-7</label>
+                        <label><input type="radio" name="ore" value="7-9" v-model="free_hours"> 7-9</label>
+                        <label><input type="radio" name="ore" value="12+" v-model="free_hours"> 12+</label>
                     </div>
                 </div>
             </div>
@@ -70,34 +70,60 @@
 </template>
 
 <script>
+    import { myaxios } from '../axios';
     export default {
         data() {
             return {
                 name: '',
                 surname: '',
                 phone: '',
-                house_type: '',
-                pets: '',
+                house_type: "apartament",
+                pets: "Nu",
                 cost_agreement: false,
-                free_hours: '',
+                free_hours: "3-4",
+            }
+        },
+        computed: {
+            id() {
+                return this.$route.params.id;
             }
         },
         methods: {
             submitFormHandler() {
+                const newForm = {
+                    name: this.name,
+                    surname: this.surname,
+                    phone: this.phone,
+                    house_type: this.house_type,
+                    pets: this.pets,
+                    cost_agreement: this.cost_agreement,
+                    free_hours: this.free_hours,
+                }
+                
+                if(this.name == '' || this.surname == '' || this.phone == '' || this.house_type == ''){
+                    
+                }
+
                 this.name = '';
                 this.surname = '';
                 this.phone = '';
-                this.location = '';
-                this.stayathome = '';
-                this.petbefore = 0;
-                this.costs = false;
+                this.house_type = "apartament";
+                this.pets = "Nu";
+                this.cost_agreement = false;
+                this.free_hours = "3-4"; 
+
+                myaxios.post(`/pet/${this.id}/adopt`,newForm).then(
+                    (data) => {
+                        console.log(data);
+                    }
+                )
             }
         },
     }
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kalam:wght@300;400;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400..700;1,400..700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Rakkas&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
     .grid {
         display: grid;
         grid-template-columns: 1fr 1fr;
@@ -161,6 +187,19 @@
         box-shadow: 4px 4px rgb(186, 186, 186);
         background-color: #f2f2f2;
     }
+
+    .submit-btn:hover {
+        color: white;
+        background: rgb(53, 77, 50);
+    }
+
+    .submit-btn:active {
+        color: white;
+        box-shadow: 0 0;
+        top: 4px;
+        left: 4px;
+    }
+
     .color-p {
         color: rgb(52, 16, 58);
         font-size: 40px;
